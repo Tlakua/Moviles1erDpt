4caaf380532e6d7d8e08fb78d13d5cde8ec4ed3e
Unknown <davidtlalpachito@codequark.com>
2018-09-07 02:33:14 -0500

    XML, inicializacion de View´s
    En este proyeccto se encuentran las practicas del primer departamental

---
+++ Moviles1erDpt/app/src/main/java/com/codequark/www/moviles1erdpt/MainActivity.java
@@ -0,0 +1,50 @@
+package com.codequark.www.moviles1erdpt;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.codequark.www.moviles1erdpt.Practicas.Pct_1;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button p1;
+    Button p2;
+    Button p3;
+    Button p3_1;
+    Button p4;
+    Button p4_1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        p1 = (Button) findViewById(R.id.btn_pct1);
+        p2 = (Button) findViewById(R.id.btn_pct2);
+        p3 = (Button) findViewById(R.id.btn_pct3);
+        p3_1 = (Button) findViewById(R.id.btn_pct3_1);
+        p4 = (Button) findViewById(R.id.btn_pct4);
+        p4_1 = (Button) findViewById(R.id.btn_pct4_2);
+    }
+
+    public void btn1(View v){
+        startActivity(new Intent(this, Pct_1.class));
+    }
+
+    public void btn2(View v){
+    }
+
+    public void btn3(View v){
+    }
+
+    public void btn3_1(View v){
+    }
+
+    public void btn4(View v){
+    }
+
+    public void btn4_1(View v){
+    }
+}
---
+++ Moviles1erDpt/app/src/main/java/com/codequark/www/moviles1erdpt/Practicas/Pct_1.java
@@ -0,0 +1,15 @@
+package com.codequark.www.moviles1erdpt.Practicas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.codequark.www.moviles1erdpt.R;
+
+public class Pct_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pct_1);
+    }
+}
