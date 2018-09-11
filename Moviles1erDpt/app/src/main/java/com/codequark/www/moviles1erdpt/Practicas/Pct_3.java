36aac606cc54f24823a92f94e5b47c7e679c3ab3
Unknown <davidtlalpachito@codequark.com>
2018-09-10 22:04:29 -0500

    Pct 3 (probar)

--- Moviles1erDpt/app/src/main/java/com/codequark/www/moviles1erdpt/Practicas/Pct_3.java
+++ Moviles1erDpt/app/src/main/java/com/codequark/www/moviles1erdpt/Practicas/Pct_3.java
@@ -2,14 +2,58 @@ package com.codequark.www.moviles1erdpt.Practicas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.codequark.www.moviles1erdpt.R;
 
 public class Pct_3 extends AppCompatActivity {
 
+    TextView tv_nomNot;
+    TextView tv_por;
+    TextView tv_fecha;
+    TextView tv_noticia;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pct_3);
+
+        inicializarValores();
+    }
+
+    public void inicializarValores(){
+        tv_nomNot = (TextView) findViewById(R.id.tv_nomNot);
+        tv_por = (TextView) findViewById(R.id.tv_por);
+        tv_fecha = (TextView) findViewById(R.id.tv_fecha);
+        tv_noticia = (TextView) findViewById(R.id.tv_noticia);
+
+        tv_nomNot.setText("Noticia num 1");
+        tv_por.setText("publicador num 1");
+        tv_fecha.setText("fecha 1");
+        tv_noticia.setText("aqui debera ir una noticia 1");
+    }
+
+    public void cambiarValores(){
+        tv_nomNot = (TextView) findViewById(R.id.tv_nomNot);
+        tv_por = (TextView) findViewById(R.id.tv_por);
+        tv_fecha = (TextView) findViewById(R.id.tv_fecha);
+        tv_noticia = (TextView) findViewById(R.id.tv_noticia);
+
+        tv_nomNot.setText("Noticia num 2");
+        tv_por.setText("publicador num 2");
+        tv_fecha.setText("fecha 2");
+        tv_noticia.setText("aqui debera ir una noticia 2");
+    }
+
+    public void btn_cambiar(View v){
+        int i = 0;
+        if (i == 0){
+            cambiarValores();
+            i =+1;
+        }if (i == 1){
+            inicializarValores();
+            i =- 1;
+        }
     }
 }
