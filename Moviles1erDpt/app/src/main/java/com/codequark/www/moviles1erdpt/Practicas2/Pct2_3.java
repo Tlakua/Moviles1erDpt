b3f61e094a3c61603186f7bd500a99efe97e72b0
Unknown <davidtlalpachito@codequark.com>
2018-10-18 23:00:31 -0500

    Pct2_3
    faltan los SP

--- Moviles1erDpt/app/src/main/java/com/codequark/www/moviles1erdpt/Practicas2/Pct2_3.java
+++ Moviles1erDpt/app/src/main/java/com/codequark/www/moviles1erdpt/Practicas2/Pct2_3.java
@@ -1,16 +1,22 @@
 package com.codequark.www.moviles1erdpt.Practicas2;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.codequark.www.moviles1erdpt.R;
 
-import java.util.Random;
 
 public class Pct2_3 extends AppCompatActivity {
 
@@ -18,8 +24,8 @@ public class Pct2_3 extends AppCompatActivity {
     private TextView tv_preg, tv_int, tv_ultima;
     private RadioButton rb_res1, rb_res2, rb_res3;
     String[] pregs, resCor, resInc1, resInc2;
-    private int i = 1, cont = 0,  randRB = 0;
-    private Random rand;
+    private int i = 0, cont = 0,  intento = 0;
+    private RadioGroup rbGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +33,16 @@ public class Pct2_3 extends AppCompatActivity {
         setContentView(R.layout.activity_pct2_3);
 
         initViews();
-        prepareComponents();
+        setPregYRb();
+        tv_ultima.setText("" + cont);
+        tv_int.setText("" + intento);
+        btn_accion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                contador();
+                valid();
+            }
+        });
     }
 
     private void initViews(){
@@ -38,6 +53,7 @@ public class Pct2_3 extends AppCompatActivity {
         rb_res2 = findViewById(R.id.rb_res2_pct2_3);
         rb_res3 = findViewById(R.id.rb_res3_pct2_3);
         tv_preg = findViewById(R.id.tv_preg_pct2_3);
+        rbGroup = findViewById(R.id.rbg_pct2_3);
 
         pregs = getResources().getStringArray(R.array.pct2_3_pregs);
         resCor = getResources().getStringArray(R.array.pct2_3_respCo);
@@ -45,30 +61,81 @@ public class Pct2_3 extends AppCompatActivity {
         resInc2 = getResources().getStringArray(R.array.pct2_3_respInc2);
     }
 
-    private void prepareComponents(){
-        btn_accion.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                btn_accion();
+    private void setPregYRb(){
+        tv_preg.setText(pregs[i]);
+        rb_res1.setText(resCor[i]);
+        rb_res2.setText(resInc1[i]);
+        rb_res3.setText(resInc2[i]);
+    }
+
+    private void valid(){
+        boolean cheked1, cheked2, cheked3;
+        cheked1 = rb_res1.isChecked();
+        cheked2 = rb_res2.isChecked();
+        cheked3 = rb_res3.isChecked();
+        if ((cheked1)||(cheked2)||(cheked3)){
+            if (i < 5){
+                i += 1;
+                setPregYRb();
+                rbGroup.clearCheck();
+            }else{
+                rbGroup.clearCheck();
+                i = 0;
+                intento += 1;
+                tv_ultima.setText("" + cont);
+                tv_int.setText("" + intento);
+                Toast.makeText(getApplicationContext(), "Aqui termina el cuestionario", Toast.LENGTH_SHORT).show();
+                dialogFinal();
+                cont = 0;
+                setPregYRb();
             }
-        });
-    }
-
-    private void btn_accion(){
-        pregunta();
-    }
-
-    private void pregunta(){
-        if(i < 6){
-            tv_preg.setText(pregs[i]);
-            i += 1;
         }else{
-            finalEncuesta();
-            i = 0;
+            Toast.makeText(getApplicationContext(), "Seleccione '1' RadioButton", Toast.LENGTH_SHORT).show();
         }
     }
 
-    private void finalEncuesta(){
-        Toast.makeText(this, "termino la encuesta con :" + cont, Toast.LENGTH_SHORT).show();
+    private void contador(){
+        switch (i){
+            case 0:
+                if (rb_res1.isChecked()){ cont += 1; } break;
+            case 1:
+                if (rb_res1.isChecked()){ cont += 1; } break;
+            case 2:
+                if (rb_res1.isChecked()){ cont += 1; } break;
+            case 3:
+                if (rb_res1.isChecked()){ cont += 1; } break;
+            case 4:
+                if (rb_res1.isChecked()){ cont += 1; } break;
+            case 5:
+                if (rb_res1.isChecked()){ cont += 1; } break;
+        }
     }
+
+    private void dialogFinal(){
+        final Dialog dialogo = new Dialog (this);
+        dialogo.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialogo.setCancelable(false);
+        dialogo.getWindow().setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+        dialogo.setContentView(R.layout.dialog_pct2_3);
+
+        TextView tv_dialog_cont = dialogo.findViewById(R.id.tv_dialog_pct2_3_cont);
+        Button btn_dialog = dialogo.findViewById(R.id.btn_dialog_pct2_3_1);
+
+        tv_dialog_cont.setText("" + cont);
+        btn_dialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialogo.cancel();
+            }
+        });
+
+        dialogo.dismiss();
+        dialogo.show();
+    }
+
+    private void setSP(){
+        SharedPreferences prefs = getSharedPreferences("MisPreferencias", Context.MODE_PRIVATE);
+        
+    }
+
 }
